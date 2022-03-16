855e32572d7837d6232f8449cc37e45e68140834
evgeniy-web-dev <vem-89@mail.ru>
2022-03-16 21:40:20 +0300

    feat: add deploy momentum

---
+++ momentum/js/lang.js
@@ -0,0 +1,28 @@
+export const lang = {
+	'en': {
+		'language': 'language',
+		'en': 'en',
+		'ru': 'ru',
+		'imageSource': 'Image source',
+		'imageSourc': 'Image sourc',
+		'hide': 'hide blocks',
+		'player': 'Audio Player',
+		'weather': 'Weather',
+		'clocks': 'Clocks',
+		'greetings': 'Greetings',
+		'quotes': 'Quotes',
+	},
+	'ru': {
+		'language': 'язык',
+		'en': 'ан',
+		'ru': 'ру',
+		'imageSource': 'Источник изобр',
+		'imageSourc': 'Источник',
+		'hide': 'Спрятать блоки',
+		'player': 'Аудио Плейер',
+		'weather': 'Погода',
+		'clocks': 'Часы',
+		'greetings': 'Приветствие',
+		'quotes': 'Цитаты',
+	}
+}
---
+++ momentum/js/quotesEn.js
@@ -0,0 +1,74 @@
+export const quotesEn = [
+	{
+		text: "It's easy to quit smoking. I've done it hundreds of times.",
+		author: "Mark Twain"
+	},
+	{
+		text: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+		author: "Thomas Edison"
+	},
+	{
+		text: "When inspiration does not come to me, I go halfway to meet it. Sigmund Freud",
+		author: "Sigmund Freud"
+	},
+	{
+		text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+		author: "Albert Einstein"
+	},
+	{
+		text: "Illusion is the first of all pleasures.",
+		author: "Oscar Wilde"
+	},
+	{
+		text: "Success doesn't come to you… you go to it.",
+		author: "Marva Collins"
+	},
+	{
+		text: "Music is the soul of language.",
+		author: "Maksud Gender"
+	},
+	{
+		text: "Well begun is half done.",
+		author: "Aristotle"
+	},
+	{
+		text: "Life is a learning experience, only if you learn.",
+		author: "Yogi Berra"
+	},
+	{
+		text: "Self-complacency is fatal to progress.",
+		author: "Margaret Sangster"
+	},
+	{
+		text: "Peace comes from within. Do not seek it without.",
+		author: "Buddha"
+	},
+	{
+		text: "What you give is what you get.",
+		author: "Byron Pulsifer"
+	},
+	{
+		text: "We can only learn to love by loving.",
+		author: "Iris Murdoch"
+	},
+	{
+		text: "Life is change. Growth is optional. Choose wisely.",
+		author: "Karen Clark"
+	},
+	{
+		text: "You'll see it when you believe it.",
+		author: "Wayne Dyer"
+	},
+	{
+		text: "Today is the tomorrow we worried about yesterday.",
+		author: null
+	},
+	{
+		text: "It's easier to see the mistakes on someone else's paper.",
+		author: null
+	},
+	{
+		text: "Every man dies. Not every man really lives.",
+		author: null
+	}
+]
